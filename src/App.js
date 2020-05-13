ab343e2f4fee1ce790fb5b6bb53a926258fa0c56
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-05-13 17:16:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Ingresar Producto"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput} />
+            <button type="submit">Agregar</button>
+          </form>
+        </header>
+      </div>
+    )
+  }
 }
-
 export default App;
