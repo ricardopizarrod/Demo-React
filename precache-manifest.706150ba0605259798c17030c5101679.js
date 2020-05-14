12bb58d3994a532f657f4489006b3b570dca43e1
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-05-14 11:51:18 -0400

    Updates

---
+++ precache-manifest.706150ba0605259798c17030c5101679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddeb59cc994adf8e39cd81a9f67ac215",
+    "url": "/ricardopizarrod/Demo-React/index.html"
+  },
+  {
+    "revision": "d288476883b65c6fd39d",
+    "url": "/ricardopizarrod/Demo-React/static/css/main.619f34b3.chunk.css"
+  },
+  {
+    "revision": "4557bc2f8b757aa34c32",
+    "url": "/ricardopizarrod/Demo-React/static/js/2.81de4433.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ricardopizarrod/Demo-React/static/js/2.81de4433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d288476883b65c6fd39d",
+    "url": "/ricardopizarrod/Demo-React/static/js/main.1637b95c.chunk.js"
+  },
+  {
+    "revision": "18f955298762d00baa44",
+    "url": "/ricardopizarrod/Demo-React/static/js/runtime-main.5d9292a8.js"
+  }
+]);
